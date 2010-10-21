7ad48431244fc96dd86e9c988785c23fede650b6
suylong <khou.suylong@gmail.com>
2010-10-21 16:08:53 +0700

    first commit

---
+++ widget_list.js
@@ -0,0 +1,3 @@
+var model = new YK.TreeStore();	
+	var accordion = model.append(null, {"display": "Gradient", "src":"javascript/gradient/gradient.js"});
+	var buttonWidget = model.append(null, {"display": "Box Shadow", "src":"button/button.js"});
